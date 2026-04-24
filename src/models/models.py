5aa5b7c85390ee3d08ad2ac883ff56213b94f8ab
borislav-vucicevic-04 <borislavvucicevic516@gmail.com>
2026-04-25 01:07:26 +0200

    Resolving issues #22 & #23

--- src/models/models.py
+++ src/models/models.py
@@ -1,11 +1,22 @@
 from dataclasses import dataclass
+from typing import Literal
 
 @dataclass(kw_only=True)
 class CreateMseStepsParams:
-    precision: float
-    weights: list[float]
-    dataset: list[list[float]]
-    real_values: list[float]
-    predicted_values: list[float]
-    errors: list[float]
-    mse: float
+  precision: float
+  weights: list[float]
+  dataset: list[list[float]]
+  real_values: list[float]
+  predicted_values: list[float]
+  errors: list[float]
+  mse: float
+
+@dataclass(kw_only=True)
+class PrintSettings:
+  page_orientation: Literal["horizontal", "vertical"] = "vertical"
+  page_size: Literal["A3", "A4", "A5", "B4", "B5", "letter", "legal", "ledger"] = "A4"
+  margin_left: float = 1
+  margin_top: float = 1
+  margin_right: float = 1
+  margin_bottom: float = 1
+  units: Literal["cm", "inch"] = "inch"
